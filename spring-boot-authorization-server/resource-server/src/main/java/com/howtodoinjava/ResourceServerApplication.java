cfd2867b81d19a72fccf536deb073dec50b92b98
lokeshgupta1981 <howtodoinjava@gmail.com>
2023-06-05 12:34:49 +0530

    Spring Native Authorization Server

---
+++ spring-boot-authorization-server/resource-server/src/main/java/com/howtodoinjava/ResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.howtodoinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResourceServerApplication.class, args);
+  }
+}
---
+++ spring-boot-authorization-server/resource-server/src/main/java/com/howtodoinjava/model/UserProfile.java
@@ -0,0 +1,38 @@
+package com.howtodoinjava.model;
+
+public class UserProfile {
+  private String name;
+  private String email;
+
+  public UserProfile() {
+  }
+
+  public UserProfile(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public String toString() {
+    return "UserProfile{" +
+        "name='" + name + '\'' +
+        ", email='" + email + '\'' +
+        '}';
+  }
+}
